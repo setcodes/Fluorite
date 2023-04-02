d4c05d15497f262e7652ab3f925910458b2e3f5d
Egor Maximov <maximov.devs@gmail.com>
2023-04-02 14:19:21 +0300

    render menu

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,11 @@
 const HABBIT_KEY = 'HABBIT_KEY';
 let habbits = [];
 
+//page
+const page = {
+	menu: document.querySelector('.menu__list'),
+};
+
 // utils
 
 function loadData() {
@@ -16,6 +21,40 @@ function saveData() {
 	localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
 }
 
+//render
+function renderMenu(activeHabbit) {
+	if (!activeHabbit) {
+		return;
+	}
+	for (const habbit of habbits) {
+		const existed = document.querySelector(`[menu-habbit-id="${habbit.id}"]`);
+		if (!existed) {
+			const element = document.createElement('button');
+			element.setAttribute('menu-habbit-id', habbit.id);
+			element.classList.add('menu__item');
+			element.addEventListener('click', () => render(habbit.id));
+			element.innerHTML = `<img src="/assets/images/${habbit.icon}.svg" alt="${habbit.name}"  />`;
+			if (activeHabbit.id === habbit.id) {
+				element.classList.add('menu__item--active');
+			}
+			page.menu.appendChild(element);
+			continue;
+		}
+		if (activeHabbit.id === habbit.id) {
+			existed.classList.add('menu__item--active');
+		} else {
+			existed.classList.remove('menu__item--active');
+		}
+	}
+}
+
+function render(activeHabbitId) {
+	const activeHabbit = habbits.find((habbit) => habbit.id === activeHabbitId);
+	renderMenu(activeHabbit);
+}
+
+// init
 (() => {
 	loadData();
+	render(habbits[0].id);
 })();
